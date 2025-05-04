14304c57ce3ee72eb9b3d9388afa05b12627fd82
ss <huotengbin@foxmail.com>
2025-05-04 11:00:10 +0800

    Add files via upload

---
+++ js/faces.js
@@ -0,0 +1,87 @@
+// Neutral faces 
+var Neutral_Face = [
+    'imgs/CH_N_F_01.jpg',
+    'imgs/CH_N_F_02.jpg',
+    'imgs/CH_N_F_03.jpg',
+    'imgs/CH_N_F_04.jpg',
+    'imgs/CH_N_F_05.jpg',
+    'imgs/CH_N_F_06.jpg',
+    'imgs/CH_N_F_07.jpg',
+    'imgs/CH_N_F_08.jpg',
+    'imgs/CH_N_M_01.jpg',
+    'imgs/CH_N_M_02.jpg',
+    'imgs/CH_N_M_03.jpg',
+    'imgs/CH_N_M_04.jpg',
+    'imgs/CH_N_M_05.jpg',
+    'imgs/CH_N_M_06.jpg',
+    'imgs/CH_N_M_07.jpg',
+    'imgs/CH_N_M_08.jpg'
+];
+
+// Neutral faces 
+var Painful_Face = [
+    'imgs/CH_P_F_01.jpg',
+    'imgs/CH_P_F_02.jpg',
+    'imgs/CH_P_F_03.jpg',
+    'imgs/CH_P_F_04.jpg',
+    'imgs/CH_P_F_05.jpg',
+    'imgs/CH_P_F_06.jpg',
+    'imgs/CH_P_F_07.jpg',
+    'imgs/CH_P_F_08.jpg',
+    'imgs/CH_P_M_01.jpg',
+    'imgs/CH_P_M_02.jpg',
+    'imgs/CH_P_M_03.jpg',
+    'imgs/CH_P_M_04.jpg',
+    'imgs/CH_P_M_05.jpg',
+    'imgs/CH_P_M_06.jpg',
+    'imgs/CH_P_M_07.jpg',
+    'imgs/CH_P_M_08.jpg'
+];
+
+// Happy faces
+var Happy_Face = [
+    'imgs/CH_H_F_01.jpg',
+    'imgs/CH_H_F_02.jpg',
+    'imgs/CH_H_F_03.jpg',
+    'imgs/CH_H_F_04.jpg',
+    'imgs/CH_H_F_05.jpg',
+    'imgs/CH_H_F_06.jpg',
+    'imgs/CH_H_F_07.jpg',
+    'imgs/CH_H_F_08.jpg',
+    'imgs/CH_H_M_01.jpg',
+    'imgs/CH_H_M_02.jpg',
+    'imgs/CH_H_M_03.jpg',
+    'imgs/CH_H_M_04.jpg',
+    'imgs/CH_H_M_05.jpg',
+    'imgs/CH_H_M_06.jpg',
+    'imgs/CH_H_M_07.jpg',
+    'imgs/CH_H_M_08.jpg'
+];
+
+// Negative Empathy Questions 
+var QuestionsN = [
+    'imgs/N1_cognitive_empathy_negative.jpg',
+    'imgs/N2_affective_empathy_negative.jpg',
+    'imgs/N3_prosocial_conform.jpg',
+    'imgs/N4_prosocial_help.jpg',
+    'imgs/N5_prosocial_donate_negative.jpg',
+    'imgs/N6_cognitive_similarity.jpg',
+    'imgs/N7_IOS.jpg'
+];
+
+// Positive Empathy Questions 
+var QuestionsP = [
+    'imgs/P1_cognitive_empathy_positive.jpg',
+    'imgs/P2_affective_empathy_positive.jpg',
+    'imgs/P3_prosocial_zhuhe.jpg',
+    'imgs/P4_prosocial_cooperation.jpg',
+    'imgs/P5_prosocial_help.jpg',
+    'imgs/P6_prosocial_donate_positive.jpg',
+    'imgs/P7_cognitive_similarity.jpg',
+    'imgs/P8_IOS.jpg'
+];
+
+face_pair = [
+    [1,1],
+    [1,2]
+];
